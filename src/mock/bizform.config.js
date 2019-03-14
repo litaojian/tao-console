7b97337f49e5dbd84e3b0f95fadfc7c4bc152587
LiTaojian <litaojian@139.com>
2019-03-14 19:47:46 +0800

    bizform

--- src/mock/bizform.config.js
+++ src/mock/bizform.config.js
@@ -4,10 +4,26 @@ const formList = {
   'testRec': {
     'restUrl': '/testRec',
     'title': 'testRec',
-    'formFields': [
-      { 'label': 'ID', name: 'id', type: 'number' },
-      { 'label': 'Title', name: 'title', type: 'string' },
-      { 'label': 'Publish Date', name: 'publish_date', type: 'date' }
+    'rules': {
+      'title': [{ required: true, message: '请输入标题' }]
+    },
+    'formLayout': [
+      [
+        { 'label': '标题', name: 'title', type: 'string', colspan: 12 },
+        { 'label': '序号', name: 'id', type: 'number', colspan: 12 }
+      ],
+      [
+        { 'label': '状态', name: 'status', type: 'select', value: '1', options: [{ label: '选项1', value: 'close' }, { label: '草稿', value: 'draft' }] },
+        { 'label': '重要性', name: 'important', type: 'select', placeholder: '重要性', dataUrl: '/api/datadict/abc' }
+      ],
+      [
+        { 'label': '选项', name: 'resource', type: 'radio', options: [{ label: '选项1', value: 1 }, { label: '选项2', value: 2 }] },
+        { 'label': '能力', name: 'options', type: 'checkbox', options: [{ label: '能力1', value: 1 }, { label: '能力2', value: 2 }] }
+      ],
+      [
+        { 'label': '发布日期', name: 'publish_date', type: 'date' },
+        { 'label': '结束日期', name: 'finish_date', type: 'date' }
+      ]
     ]
   }
 }
--- src/views/bizform/form.vue
+++ src/views/bizform/form.vue
@@ -1,44 +1,27 @@
 <template>
-  <div class="createPost-container">
-    <el-form ref="mainForm" :model="mainForm" :rules="rules" class="form-container">
+  <div class="bizForm-container">
+    <el-form ref="bizForm" :model="bizForm" :rules="rules" :inline="true" class="form-container">
 
-      <sticky :class-name="'sub-navbar '+ mainForm.status">
-        <el-button v-loading="loading" style="margin-left: 10px;" type="success" @click="submitForm">发布
+      <sticky :class-name="'sub-navbar '+ bizForm.status">
+        <el-button v-loading="loading" style="margin-left: 10px;" type="success" @click="submitForm">提交
         </el-button>
-        <el-button v-loading="loading" type="warning" @click="submitForm">草稿</el-button>
       </sticky>
 
-      <div class="createPost-main-container">
-        <el-row>
-
-          <el-col :span="24">
-            <el-form-item style="margin-bottom: 40px;" prop="title">
-              <MDinput v-model="mainForm.title" :maxlength="100" name="name" required>
-                标题1
-              </MDinput>
+      <div class="bizForm-main-container">
+        <el-row v-for="(layoutRow,rowIndex) in formLayout" :key="rowIndex">
+          <el-col v-for="(field,columnIndex) in layoutRow" :key="columnIndex" :span="field.colspan || 12" >
+            <el-form-item :prop="field.name" :label="field.label">
+              <el-input v-if="(field.type === null || field.type === 'string')" v-model="bizForm[field.name]" :maxlength="field.maxlength || 10" :name="field.name" :required="field.required"/>
+              <el-input v-if="field.type === 'number'" v-model="bizForm[field.name]" :maxlength="field.maxlength || 10" :name="field.name" :required="field.required" type="number"/>
+              <el-date-picker v-if="field.type === 'datetime'" v-model="bizForm[field.name]" type="datetime" format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间"/>
+              <el-date-picker v-if="field.type === 'date'" v-model="bizForm[field.name]" type="date" format="yyyy-MM-dd" placeholder="选择日期"/>
+              <el-select v-if="field.type === 'select'" v-model="bizForm[field.name]" :placeholder="field.placeholder || '请选择'">
+                <el-option v-for="item in field.options" :label="item.label" :key="item.value" :value="item.value"/>
+              </el-select>
+              <el-radio-group v-if="field.type === 'radio'" v-model="bizForm[field.name]">
+                <el-radio v-for="(item,index) in field.options" :label="item.label" :key="index" :value="item.value"/>
+              </el-radio-group>
             </el-form-item>
-
-            <div class="postInfo-container">
-              <el-row>
-                <el-col :span="10">
-                  <el-form-item label-width="80px" label="发布时间:" class="postInfo-container-item">
-                    <el-date-picker v-model="mainForm.display_time" type="datetime" format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间"/>
-                  </el-form-item>
-                </el-col>
-
-                <el-col :span="6">
-                  <el-form-item label-width="60px" label="重要性:" class="postInfo-container-item">
-                    <el-rate
-                      v-model="mainForm.importance"
-                      :max="3"
-                      :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
-                      :low-threshold="1"
-                      :high-threshold="3"
-                      style="margin-top:8px;"/>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-            </div>
           </el-col>
         </el-row>
 
@@ -49,7 +32,7 @@
 </template>
 
 <script>
-import { getBizFormConfig, getAction } from '@/api/bizform'
+import { getBizFormConfig, getAction, postAction } from '@/api/bizform'
 import { validateURL } from '@/utils/validate'
 import MDinput from '@/components/MDinput'
 import Sticky from '@/components/Sticky' // 粘性header组件
@@ -72,18 +55,28 @@ export default {
   name: 'BizForm',
   components: { MDinput, Sticky },
   data() {
-    const validateRequire = (rule, value, callback) => {
-      if (value === '') {
-        this.$message({
-          message: rule.field + '为必传项',
-          type: 'error'
-        })
-        callback(new Error(rule.field + '为必传项'))
-      } else {
-        callback()
-      }
+    return {
+      bizForm: Object.assign({}, defaultForm),
+      rules: {},
+      tempRoute: {},
+      loading: false,
+      formName: null,
+      formLayout: [],
+      rowId: undefined,
+      bizConfig: null,
+      listLoading: true,
+      restUrl: null,
+      options: [{ 'lable': 'T1', 'value': 1 }, { 'lable': 'T2', 'value': 2 }, { 'lable': '草稿', 'value': 'draft' }]
     }
-    const validateSourceUri = (rule, value, callback) => {
+  },
+  created() {
+    // debugger
+    this.formName = this.$route.params && this.$route.params.formname
+    this.rowId = this.$route.params && this.$route.params.rowid
+    this.getConfig()
+  },
+  methods: {
+    validateSourceUri(rule, value, callback) {
       if (value) {
         if (validateURL(value)) {
           callback()
@@ -97,36 +90,24 @@ export default {
       } else {
         callback()
       }
-    }
-    return {
-      mainForm: Object.assign({}, defaultForm),
-      rules: {
-        image_uri: [{ validator: validateRequire }],
-        title: [{ validator: validateRequire }],
-        content: [{ validator: validateRequire }],
-        source_uri: [{ validator: validateSourceUri, trigger: 'blur' }]
-      },
-      tempRoute: {},
-      loading: false,
-      formName: null,
-      rowId: undefined,
-      bizConfig: null,
-      listLoading: true,
-      restUrl: null
-    }
-  },
-  created() {
-    // debugger
-    this.formName = this.$route.params && this.$route.params.formname
-    this.rowId = this.$route.params && this.$route.params.rowid
-    this.getConfig()
-  },
-  methods: {
+    },
+    validateRequire(rule, value, callback) {
+      if (value === '') {
+        this.$message({
+          message: rule.field + '为必传项',
+          type: 'error'
+        })
+        callback(new Error(rule.field + '为必传项'))
+      } else {
+        callback()
+      }
+    },
     getConfig() {
       getBizFormConfig(this.formName).then(response => {
         this.bizConfig = response.data.config
         this.restUrl = this.bizConfig.restUrl
-        this.formFields = this.bizConfig.formFields
+        this.rules = this.bizConfig.rules
+        this.formLayout = this.bizConfig.formLayout
         this.listLoading = false
         // console.log('bizFormConfig: ' + JSON.stringify(this.bizConfig))
         if (this.actionUrl) {
@@ -143,17 +124,30 @@ export default {
       })
     },
     submitForm() {
-      console.log(this.mainForm)
-      this.$refs.mainForm.validate(valid => {
+      console.log('Form Data:' + JSON.stringify(this.bizForm))
+      this.$refs.bizForm.validate(valid => {
         if (valid) {
           this.loading = true
-          this.$notify({
-            title: '成功',
-            message: '保存成功',
-            type: 'success',
-            duration: 2000
+          postAction(this.restUrl, this.bizForm).then(response => {
+            console.log(this.restUrl + ' POST result :' + response)
+            if (response.errcode === 0) {
+              this.$notify({
+                title: '成功',
+                message: '保存成功',
+                type: 'success',
+                duration: 2000
+              })
+            } else {
+              this.$notify({
+                title: '失败',
+                message: '保存失败',
+                type: 'warn',
+                duration: 2000
+              })
+            }
           })
-          // this.mainForm.status = 'published'
+
+          // this.bizForm.status = 'published'
           this.loading = false
         } else {
           console.log('error submit!!')
@@ -167,9 +161,9 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 @import "~@/styles/mixin.scss";
-.createPost-container {
+.bizForm-container {
   position: relative;
-  .createPost-main-container {
+  .bizForm-main-container {
     padding: 40px 45px 20px 50px;
     .postInfo-container {
       position: relative;
